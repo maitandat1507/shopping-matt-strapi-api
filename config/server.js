8d5aa7c30d2f2e1c261d340d54ed76f53a6bf0d1
maitandat1507 <maitandat1507@gmail.com>
2021-08-30 15:47:46 +0700

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df199635c0d0734350c477bc4fa935d1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b6ff04f8-c0d4-409b-9be1-4dd8d3c7cdf3'
+};
